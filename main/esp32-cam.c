ab1061e6fb9bd2b14d4b5eebaca3bbef8d13bb7d
Shuai Wang <wang.lin.shuai@qq.com>
2026-01-03 21:25:44 +0800

    feat: add winfi and mdns

--- main/esp32-cam.c
+++ main/esp32-cam.c
@@ -1,6 +1,37 @@
-#include <stdio.h>
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_mac.h"
+#include "esp_wifi.h"
+#include "mdns.h"
+#include "nvs_flash.h"
+#include "protocol_examples_common.h"
 
+#define TAG "esp32-cam"
+
+static void start_mdns_service(void)
+{
+    const char * hostname = "esp32-cam";
+    // initialize mDNS
+    ESP_ERROR_CHECK(mdns_init());
+    // set mDNS hostname (required if you want to advertise services)
+    ESP_ERROR_CHECK(mdns_hostname_set(hostname));
+    ESP_LOGI(TAG, "mdns hostname set to: [%s].locale", hostname);
+}
+
+static void stop_mdns_service(void)
+{
+    mdns_free();
+}
 void app_main(void)
 {
-    printf("hello,esp\n");
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
+    ESP_ERROR_CHECK(example_connect());
+    start_mdns_service();
 }
